8186fb2b51822e0a85778722bf0a326699a28821
Shuprov <asifurshuprov@gmail.com>
2021-06-30 01:21:57 +0600

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*Route::get('/login',function()
+{
+    return view('login');
+}); */
+Route::get('/login' ,['uses' => 'LoginController@index']);
+//Route::get('/login' , 'LoginController@index');
+
+/*Route::post('/login',function()
+{
+    echo "posted";
+}); */
+Route::post('/login','LoginController@verify');
+
+Route::get('/register','RegistrationController@index');
+Route::post('/register','RegistrationController@verify');
+
+Route::get('/admin',[App\Http\Controllers\AdminController::class,'index']);
+Route::get('/logout','LogoutController@index');
+
+Route::get('/user','UserController@index');
+//Route::post('/user','UserController@index');
+ Route::get('/create','UserController@create');
+ Route::post('/create','UserController@verify');
+
+
+ Route::get('/edit/{id}','UserController@edit');
+ Route::post('/edit/{id}','UserController@update');
+
+ Route::get('/delete/{id}','UserController@delete');
+ Route::post('/delete/{id}','UserController@destroy');
+
+ //Route::get('/create','UserController@submit');
+ Route::post('/create','UserController@submit');
+
+ Route::get('/delete/{id}', 'UserController@user_delete')->name("user_delete");
+
+
+
+
+
